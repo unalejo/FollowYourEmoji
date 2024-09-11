b270b822731c647b0ff8d05bcd97f0025e1f9c31
Alejandro Sepúlveda <alejo8d@gmail.com>
2024-09-11 16:57:57 -0500

    organizando

--- infer.sh
+++ infer.sh
@@ -6,6 +6,6 @@ CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.run \
     --nproc_per_node 1 \
     infer.py \
     --config ./configs/infer.yaml \
-    --input_path 2024-09-10_09-11-07_6467.png \
-    --lmk_path 20240911_074529_mppose.npy \
+    --input_path ./input/2024-09-10_09-11-07_6467.png \
+    --lmk_path ./input/20240911_074529_mppose.npy \
     --output_path ./output/
