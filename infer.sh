b14aa6b48d2d48f04d45e90c514bc140439e383a
Alejandro Sepúlveda <alejo8d@gmail.com>
2024-09-11 15:59:05 -0500

    Corriendo localmente

--- infer.sh
+++ infer.sh
@@ -1,11 +1,11 @@
 CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.run \
     --nnodes 1 \
-    --master_addr $LOCAL_IP \
+    --master_addr localhost  \
     --master_port 12345 \
     --node_rank 0 \
     --nproc_per_node 1 \
     infer.py \
     --config ./configs/infer.yaml \
-    --input_path ./data/images \
-    --lmk_path ./data/mplmks \
-    --output_path ./data/out
+    --input_path 2024-09-10_09-11-07_6467.png \
+    --lmk_path 20240911_074529_mppose.npy \
+    --output_path ./output/
